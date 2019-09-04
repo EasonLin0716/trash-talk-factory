1d440415f63ba08e474aacb5e6a913164d7e71bc
Eason Lin <uu9924079@gmail.com>
2019-09-04 20:59:41 +0800

    update handlebar helper == to ===

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const trashTalk = require('./trash_talk_factory')
 // https://stackoverflow.com/questions/34252817/handlebarsjs-check-if-a-string-is-equal-to-a-value
 const Handlebars = require("handlebars");
 Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
-  return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+  return arg1 === arg2 ? options.fn(this) : options.inverse(this);
 });
 
 // setting template engine
